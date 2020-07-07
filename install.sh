933d6e9fc1c1cef4a9323cb0cdcbcf3bb3d759dc
TheJoeCoder <joe@radialbog9.uk>
2020-07-07 20:51:51 +0100

    added stuff

--- install.sh
+++ install.sh
@@ -6,23 +6,26 @@ echo "# by Radialbog9      #"
 echo "######################"
 
 x86onpi_logger () {
-	echo "[x86OnPi] $1"
+	echo "[x86OnPi] $1" 1>&2
 	echo "[x86OnPi] $1" >> "install.log"
 }
 
 if [ "$(id -u)" != "0" ]; then
-   echo "[ERROR] This script must be run as root or with sudo." 1>&2
+   echo "[x86OnPi] Error: This script must be run as root or with sudo."
    exit 1
 fi
+
+rm "install.log"
+
 DEBIAN=no
 case "`/usr/bin/lsb_release -si`" in
-	Debian) echo "Debian found." && DEBIAN=yes ;;
-	Raspbian) echo "Raspbian found." && DEBIAN=yes ;;
-	Ubuntu) echo "Ubuntu found." && DEBIAN=yes ;;
-	*) echo "Operating system not supported." && DEBIAN=no ;;
+	Debian) x86onpi_logger "Compatible OS Debian found." && DEBIAN=yes ;;
+	Raspbian) x86onpi_logger "Compatible OS Raspbian found." && DEBIAN=yes ;;
+	Ubuntu) x86onpi_logger "Compatible OS Ubuntu found." && DEBIAN=yes ;;
+	*) x86onpi_logger "OS '`/usr/bin/lsb_release -si`' is not supported." && DEBIAN=no ;;
 esac
 if [ "$DEBIAN" = "no" ]; then
-	echo "[ERROR] Your operating system is not supported."
+	x86onpi_logger "Error: Your operating system is not supported."
 	exit 0
 fi
 unset DEBIAN
@@ -34,8 +37,7 @@ WHIPTAIL=no
 fi
 if [ "$WHIPTAIL" = "no" ]; then
 	echo "[ERROR] Whiptail is required for the installation to continue. Please install whiptail."
-else
-whiptail --title "Success" --backtitle "x86OnPi Installer" --msgbox "Whiptail is installed!" 10 50
-
 fi
+
+whiptail --backtitle "x86OnPi Installer" --title "Warning!" --msgbox "x86OnPi will only work with kernels which have a 3G/1G vmsplit. Please make sure that your kernel is compiled like this." 30 10
 
